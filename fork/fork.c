01b04defd68c57d158f4813522858eed56803361
Pavel <korsun4@mail.ru>
2020-04-17 21:53:39 +0300

    add fork

---
+++ fork/fork.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid;
+    char* message;
+    int n;
+
+    printf("fork programm starting\n");
+    pid = fork();
+    switch(pid) {
+        case -1:
+            perror("fork failed");
+            exit(1);
+        case 0:
+            message = "This is the child";
+            n = 5;
+            break;
+        default:
+            message = "This is the parent";
+            n = 3;
+            break;
+    }
+
+    for(; n > 0; n--) {
+        puts(message);
+        sleep(1);
+    }
+
+    return 0;
+}
