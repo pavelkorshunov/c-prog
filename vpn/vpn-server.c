15ed055c53ba047ab7362ade0a09efcf979fac38
Pavel <korsun4@mail.ru>
2023-05-10 23:08:35 +0300

    learn linux interfaces

--- vpn/vpn-server.c
+++ vpn/vpn-server.c
@@ -12,31 +12,7 @@
 #include <fcntl.h>
 
 #ifdef __linux__
-// There are several ways to play with this program. Here we just give an
-// example for the simplest scenario. Let us say that a Linux box has a
-// public IPv4 address on eth0. Please try the following steps and adjust
-// the parameters when necessary.
-//
-// # Enable IP forwarding
-// echo 1 > /proc/sys/net/ipv4/ip_forward
-//
-// # Pick a range of private addresses and perform NAT over eth0.
-// iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
-//
-// # Create a TUN interface.
-// ip tuntap add dev tun0 mode tun
-//
-// # Set the addresses and bring up the interface.
-// ifconfig tun0 10.0.0.1 dstaddr 10.0.0.2 up
-//
-// # Create a server on port 8000 with shared secret "test".
-// ./ToyVpnServer tun0 8000 test -m 1400 -a 10.0.0.2 32 -d 8.8.8.8 -r 0.0.0.0 0
-//
-// This program only handles a session at a time. To allow multiple sessions,
-// multiple servers can be created on the same port, but each of them requires
-// its own TUN interface. A short shell script will be sufficient. Since this
-// program is designed for demonstration purpose, it performs neither strong
-// authentication nor encryption. DO NOT USE IT IN PRODUCTION!
+
 #include <net/if.h>
 #include <linux/if_tun.h>
 
@@ -69,25 +45,14 @@ static int get_interface(char *name)
 #endif
 
 int main(int argc, char **argv) {
-    if (argc < 5) {
-        printf("Usage: %s <tunN> <port> <secret> options...\n"
-               "\n"
-               "Options:\n"
-               "  -m <MTU> for the maximum transmission unit\n"
-               "  -a <address> <prefix-length> for the private address\n"
-               "  -r <address> <prefix-length> for the forwarding route\n"
-               "  -d <address> for the domain name server\n"
-               "  -s <domain> for the search domain\n"
-               "\n"
-               "Note that TUN interface needs to be configured properly\n"
-               "BEFORE running this program. For more information, please\n"
-               "read the comments in the source code.\n\n", argv[0]);
+    if (argc < 2) {
+        printf("Usage: %s <interface>\n", argv[0]);
         exit(1);
     }
 
-    // for(int i = 0; i < argc; i++) {
-    //     printf("%s\n", argv[i]);
-    // }
+    for(int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 
     return 0;
 };
