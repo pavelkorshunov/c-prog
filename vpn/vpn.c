a75e35c132b056005adb1c643e49aad7a65c9038
Pavel <korsun4@mail.ru>
2023-05-10 14:58:07 +0300

    example vpn

---
+++ vpn/vpn.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#ifdef __linux__
+// There are several ways to play with this program. Here we just give an
+// example for the simplest scenario. Let us say that a Linux box has a
+// public IPv4 address on eth0. Please try the following steps and adjust
+// the parameters when necessary.
+//
+// # Enable IP forwarding
+// echo 1 > /proc/sys/net/ipv4/ip_forward
+//
+// # Pick a range of private addresses and perform NAT over eth0.
+// iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
+//
+// # Create a TUN interface.
+// ip tuntap add dev tun0 mode tun
+//
+// # Set the addresses and bring up the interface.
+// ifconfig tun0 10.0.0.1 dstaddr 10.0.0.2 up
+//
+// # Create a server on port 8000 with shared secret "test".
+// ./ToyVpnServer tun0 8000 test -m 1400 -a 10.0.0.2 32 -d 8.8.8.8 -r 0.0.0.0 0
+//
+// This program only handles a session at a time. To allow multiple sessions,
+// multiple servers can be created on the same port, but each of them requires
+// its own TUN interface. A short shell script will be sufficient. Since this
+// program is designed for demonstration purpose, it performs neither strong
+// authentication nor encryption. DO NOT USE IT IN PRODUCTION!
+#include <net/if.h>
+#include <linux/if_tun.h>
+
+static int get_interface(char *name)
+{
+    int interface = open("/dev/net/tun", O_RDWR | O_NONBLOCK);
+    return interface;
+}
+#else
+#error Sorry, you have to implement this part by yourself.
+#endif
+
+int main() {
+    printf("Hello\n");
+};
